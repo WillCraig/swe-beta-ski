571af5302ce7de6e62183121da73a266bed1f5a7
Daniel Write <daniel@writefamily.com>
2023-02-28 22:24:23 -0500

    raw skeleton

--- sketch.js
+++ sketch.js
@@ -7,6 +7,7 @@ function currentPlaying() {
 }
 
 // the function that will draw the game menu
+// it is not actually currently handled in the game code
 function startMenu() {}
 
 // the function that will draw the pause menu
@@ -14,8 +15,26 @@ function pauseMenu() {}
 
 function setup() {
   createCanvas(400, 400);
-  startMenu();
 }
+
+// will draw the background of the game
+// it should flush the current screen such that we do not have any ghosts
+function drawBackground() {}
+
+// this hash will contain the various obstacles generated, their position, etc
+worldMatrix = []
+
+// this function will generate the next column of obstacles, enemies, etc and place it in the world matrix
+// it will be guaranteed to check against placing two items in the same space
+function generateNextColumn() {}
+
+// renders the world matrix using actual assests or colored blocks or whatever
+function drawWorld() {}
+
+// handles the various entities, including the player
+// moves them forward along in the world
+// gravity, etc
+function doPhysics() {}
 
 // the draw() function is called by p5js once per tick
 // it is the game logic loop
@@ -24,7 +43,12 @@ function draw() {
     pauseMenu();
     return;
   }
+  
+  doPhysics();
+  generateNextColumn();
+  
+  drawBackground();
+  drawWorld();
 }
 
-function keyPressed() {
-}
+function keyPressed() {}
