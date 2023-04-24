e4da0a8704bea443b2331624efb4e2d3a0080c1c
Kelly Badowski <123491579+Kbadowski@users.noreply.github.com>
2023-04-23 20:31:38 -0400

    Update obstacle.js

--- obstacle.js
+++ obstacle.js
@@ -1,17 +1,27 @@
 let lanes = [100, 300, 500];
+let rockImg;
+
+ function preload(){
+	rockImg = loadImage("rock.jpg");
+    treeImg = loadImage("tree.jpg")
+}
+//function setup(){
+ // img = random(rockImg , treeImg);
+//}
 
 class Obstacle{
-  constructor() {
-    this.b = 20; // size
+  constructor(){
+    this.b = 30; // size
     this.x = lanes[Math.floor(Math.random()*lanes.length)] // randonly choosing the lane
     this.y = 20; // height
   }
   
-  move() {
+  move(){
     this.y += 3;
   }
-  
-  display() {
-    circle(this.x, this.y, this.b, this.b);
+
+  display(){
+    image(rockImg, this.x, this.y, this.b , this.b);
+    //circle(this.x, this.y, this.b, this.b);
   }
 }
